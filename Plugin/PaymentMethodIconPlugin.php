b6c6a8237613d9ca72f19159ddc8d9e97a978a12
Jisse Reitsma <jisse@yireo.com>
2024-11-30 19:55:57 +0100

    Standardize payment icons by outputting SVG directly

--- Plugin/PaymentMethodIconPlugin.php
+++ Plugin/PaymentMethodIconPlugin.php
@@ -2,16 +2,23 @@
 
 namespace Yireo\LokiCheckoutBuckaroo\Plugin;
 
+use Magento\Framework\Component\ComponentRegistrar;
 use Yireo\LokiCheckout\ViewModel\PaymentMethodIcon;
 
 class PaymentMethodIconPlugin
 {
+    public function __construct(
+        private ComponentRegistrar $componentRegistrar
+    ) {
+    }
+
     public function afterGetIcon(
         PaymentMethodIcon $paymentMethodIcon,
         string $result,
         string $paymentMethodCode
     ): string {
-        if (false === preg_match('/^buckaroo_methods_(.*)$/', $paymentMethodCode, $match)) {
+
+        if (false === preg_match('/^buckaroo_magento2_(.*)$/', $paymentMethodCode, $match)) {
             return $result;
         };
 
@@ -19,9 +26,12 @@ class PaymentMethodIconPlugin
             return $result;
         }
 
-        $iconFileId = 'Buckaroo_Magento2::images/methods/'.$match[1].'.svg';
-        $iconUrl = $paymentMethodIcon->getAssetRepository()->getUrl($iconFileId);
+        $modulePath = $this->componentRegistrar->getPath(ComponentRegistrar::MODULE, 'Buckaroo_Magento2');
+        $iconFilePath = $modulePath . '/view/base/web/images/svg/'.$match[1].'.svg';
+        if (false === file_exists($iconFilePath)) {
+            return $result;
+        }
 
-        return '<img src="'.$iconUrl.'" />';
+        return file_get_contents($iconFilePath);
     }
 }
