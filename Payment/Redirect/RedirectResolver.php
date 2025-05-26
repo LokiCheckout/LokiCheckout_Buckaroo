34c651e05a8d764b406285b189869f83f3506893
Jisse Reitsma <jisse@yireo.com>
2025-05-26 13:54:08 +0200

    Fix issues with redirect to portal

--- Payment/Redirect/RedirectResolver.php
+++ Payment/Redirect/RedirectResolver.php
@@ -5,9 +5,15 @@ namespace Yireo\LokiCheckoutBuckaroo\Payment\Redirect;
 use Buckaroo\Magento2\Model\Method\AbstractMethod;
 use Yireo\LokiCheckout\Payment\Redirect\RedirectResolverInterface;
 use Yireo\LokiCheckout\Step\FinalStep\RedirectContext;
+use Magento\Framework\Registry;
 
 class RedirectResolver implements RedirectResolverInterface
 {
+    public function __construct(
+        private Registry $registry
+    ) {
+    }
+
     public function resolve(RedirectContext $redirectContext): false|string
     {
         $paymentMethod = $redirectContext->getPaymentMethod();
@@ -15,6 +21,27 @@ class RedirectResolver implements RedirectResolverInterface
             return false;
         }
 
-        return $paymentMethod->getOrderPlaceRedirectUrl();
+        $redirectUrl = $this->getUrlFromResponse();
+        if (!empty($redirectUrl)) {
+            return $redirectUrl;
+        }
+
+        $redirectUrl = $paymentMethod->getOrderPlaceRedirectUrl();
+        if (is_string($redirectUrl)) {
+            return $redirectUrl;
+        }
+
+        return false;
+    }
+
+
+    private function getUrlFromResponse(): string
+    {
+        $response = $this->registry->registry('buckaroo_response');
+        if (!empty($response)) {
+            return (string)$response[0]->RequiredAction->RedirectURL;
+        }
+
+        return '';
     }
 }
