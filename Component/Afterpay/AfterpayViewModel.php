bd9ccf45970be935221a9f679f9733c5c73eaae9
Jisse Reitsma <jisse@yireo.com>
2025-08-25 16:55:16 +0200

    Beautify new form

--- Component/Afterpay/AfterpayViewModel.php
+++ Component/Afterpay/AfterpayViewModel.php
@@ -12,32 +12,14 @@ class AfterpayViewModel extends FieldViewModel
 {
     public function getTermsConditionsText(): string
     {
-        return (string)$this->getContext()->getScopeConfig()->getValue('loki_checkout/buckaroo/afterpay_terms');
+        return (string)$this->getContext()->getScopeConfig()->getValue(
+            'loki_checkout/buckaroo/afterpay_terms'
+        );
     }
 
     public function isRequired(): bool
     {
         return true;
-    }
-
-    public function getPaymentMethodLabel(): string
-    {
-        return (string)$this->getContext()->getQuote()->getPayment()->getMethodInstance()->getTitle();
-    }
-
-    public function getTermsAndConditionsUrl(): string
-    {
-        return (string)$this->getContext()->getScopeConfig()->getValue('loki_checkout/buckaroo/afterpay_terms_conditions_url');
-    }
-
-    public function getPrivacyPolicyUrl(): string
-    {
-        return (string)$this->getContext()->getScopeConfig()->getValue('loki_checkout/buckaroo/afterpay_privacy_statement_url');
-    }
-
-    public function getCookieStatementUrl(): string
-    {
-        return (string)$this->getContext()->getScopeConfig()->getValue('loki_checkout/buckaroo/afterpay_cookie_statement_url');
     }
 
     public function getFieldLabel(): string
@@ -55,5 +37,64 @@ class AfterpayViewModel extends FieldViewModel
         }
 
         return '';
+    }
+
+    public function getInputLabel(): string
+    {
+        if (str_ends_with($this->getComponentName(), '.terms')) {
+            return (string)__(
+                'The general Terms and Conditions for the Riverty payment method apply. The privacy policy of Riverty can be found here.'
+            );
+        }
+
+        return '';
+    }
+
+    public function getComment(): string
+    {
+        if (str_ends_with($this->getComponentName(), '.terms')) {
+            $text = $this->getContext()->getScopeConfig()->getValue(
+                'loki_checkout/buckaroo/afterpay_terms'
+            );
+
+            return (string)
+            __(
+                $text,
+                $this->getTermsConditionsText(),
+                $this->getPaymentMethodLabel(),
+                $this->getTermsAndConditionsUrl(),
+                $this->getPrivacyPolicyUrl(),
+                $this->getCookieStatementUrl(),
+            );
+        }
+
+        return '';
+    }
+
+    private function getPaymentMethodLabel(): string
+    {
+        return (string)$this->getContext()->getQuote()->getPayment()
+            ->getMethodInstance()->getTitle();
+    }
+
+    private function getTermsAndConditionsUrl(): string
+    {
+        return (string)$this->getContext()->getScopeConfig()->getValue(
+            'loki_checkout/buckaroo/afterpay_terms_conditions_url'
+        );
+    }
+
+    private function getPrivacyPolicyUrl(): string
+    {
+        return (string)$this->getContext()->getScopeConfig()->getValue(
+            'loki_checkout/buckaroo/afterpay_privacy_statement_url'
+        );
+    }
+
+    private function getCookieStatementUrl(): string
+    {
+        return (string)$this->getContext()->getScopeConfig()->getValue(
+            'loki_checkout/buckaroo/afterpay_cookie_statement_url'
+        );
     }
 }
