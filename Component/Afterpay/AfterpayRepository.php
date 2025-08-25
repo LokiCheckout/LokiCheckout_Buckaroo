993c9dac6e647c256ca76b5bec323cf5a0265c69
Jisse Reitsma <jisse@yireo.com>
2025-08-25 16:59:55 +0200

    Add validators for afterpay

--- Component/Afterpay/AfterpayRepository.php
+++ Component/Afterpay/AfterpayRepository.php
@@ -29,21 +29,17 @@ class AfterpayRepository extends ComponentRepository
 
     public function saveValue(mixed $value): void
     {
-        if (!is_array($value)) {
-            return;
-        }
-
-        if (array_key_exists('customerIban', $value)) {
-            $this->saveProperty('customer_iban', (string)$value['customerIban']);
-        }
-
-        if (array_key_exists('customerDob', $value)) {
-            $this->saveProperty('customer_Dob', (string)$value['customerDob']);
-        }
-
-        if (array_key_exists('termsCondition', $value)) {
-            $termsCondition = (int)$value['termsCondition'] > 0 ? 1 : 0;
+        if (str_ends_with($this->getComponentName(), '.terms')) {
+            $termsCondition = (int)$value > 0 ? 1 : 0;
             $this->saveProperty('termsCondition', $termsCondition);
+        }
+
+        if (str_ends_with($this->getComponentName(), '.iban')) {
+            $this->saveProperty('customer_iban', (string)$value);
+        }
+
+        if (str_ends_with($this->getComponentName(), '.dob')) {
+            $this->saveProperty('customer_Dob', (string)$value);
         }
     }
 
