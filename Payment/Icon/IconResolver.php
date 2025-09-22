b2f20e7165f1af082a7cd75c87e811e4ddbd32ac
Jisse Reitsma <jisse@yireo.com>
2025-09-22 16:47:13 +0200

    WIP with icon attributes

--- Payment/Icon/IconResolver.php
+++ Payment/Icon/IconResolver.php
@@ -31,7 +31,7 @@ class IconResolver implements IconResolverInterface
 
         $paymentLogo = $this->infoBlock->getPaymentLogo($match[1]);
         if ($paymentLogo) {
-            return $this->imageOutput->getByUrl($paymentLogo);
+            return $this->imageOutput->getByUrl($paymentLogo, $iconResolverContext->getAttributes());
         }
 
         $iconFilePath = $iconResolverContext->getIconPath(
