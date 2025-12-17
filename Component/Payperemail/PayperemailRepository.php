f01d9e0721eb9d481437370a75cae0bc37c68e93
Jisse Reitsma <jisse@yireo.com>
2025-12-17 15:49:24 +0100

    Add payperemail method

---
+++ Component/Payperemail/PayperemailRepository.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace LokiCheckout\Buckaroo\Component\Payperemail;
+
+use LokiCheckout\Core\Component\Base\Payment\AdditionalInformation\AdditionalInformationRepository;
+
+class PayperemailRepository extends AdditionalInformationRepository
+{
+}
---
+++ Component/Payperemail/PayperemailViewModel.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace LokiCheckout\Buckaroo\Component\Payperemail;
+
+use LokiCheckout\Core\Component\Base\Generic\CheckoutContext;
+use LokiCheckout\Core\Component\Base\Payment\AdditionalInformation\AdditionalInformationViewModel;
+
+/**
+ * @method CheckoutContext getContext()
+ */
+class PayperemailViewModel extends AdditionalInformationViewModel
+{
+}
