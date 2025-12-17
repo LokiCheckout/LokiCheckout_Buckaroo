b727c396438346650b3534eb4d75295e0e7b313b
Jisse Reitsma <jisse@yireo.com>
2025-12-17 15:48:15 +0100

    Add new gender options for payperemail

---
+++ ViewModel/OptionModel/GenderOptionModel.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace LokiCheckout\Buckaroo\ViewModel\OptionModel;
+
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+use Loki\Components\Component\ComponentViewModelInterface;
+use Loki\Field\Util\OptionModelInterface;
+
+class GenderOptionModel implements ArgumentInterface, OptionModelInterface
+{
+    public function getOptions(ComponentViewModelInterface $viewModel): array
+    {
+        return [
+            '1' => __('Mr.'),
+            '2' => __('Mrs.'),
+            '0' => __('They/them'),
+            '9' => __('Not applicable'),
+        ];
+    }
+}
