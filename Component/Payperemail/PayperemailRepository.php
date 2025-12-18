81bbc76788e51e56dd4b104f110c3561dfe19f82
Jisse Reitsma <jisse@yireo.com>
2025-12-18 14:46:18 +0100

    Allow for default Payperemail values from quote

--- Component/Payperemail/PayperemailRepository.php
+++ Component/Payperemail/PayperemailRepository.php
@@ -6,4 +6,27 @@ use LokiCheckout\Core\Component\Base\Payment\AdditionalInformation\AdditionalInf
 
 class PayperemailRepository extends AdditionalInformationRepository
 {
+    public function getDefaultValue(): mixed
+    {
+        $properyName = $this->getPropertyName();
+        $quote = $this->getContext()->getQuote();
+
+        if ($properyName === 'customer_email') {
+            return $quote->getCustomerEmail();
+        }
+
+        if ($properyName === 'customer_billingFirstName') {
+            return $quote->getBillingAddress()->getFirstname();
+        }
+
+        if ($properyName === 'customer_billingMiddleName') {
+            return $quote->getBillingAddress()->getMiddlename();
+        }
+
+        if ($properyName === 'customer_billingLastName') {
+            return $quote->getBillingAddress()->getLastname().'2';
+        }
+
+        return parent::getDefaultValue();
+    }
 }
