4630047dd63b02c8a8e3d95c3bd7a56c8318bc62
Jisse Reitsma <jisse@yireo.com>
2026-04-01 19:23:06 +0200

    Remove obsolete issuer selection for Paybybank

--- Component/Paybybank/PaybybankRepository.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace LokiCheckout\Buckaroo\Component\Paybybank;
-
-use LokiCheckout\Core\Component\Base\Payment\AdditionalInformation\AdditionalInformationRepository;
-
-class PaybybankRepository extends AdditionalInformationRepository
-{
-}
